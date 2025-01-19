d02c1a91f4be0f7bd3e10d9091f010ddaada72e1
Abdul Wasay <irfansoomro370@gmail.com>
2025-01-19 07:50:31 +0500

    new

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import {assets} from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
-import Cart from '../pages/cart'
+import Cart from '../pages/Cart'
 import { ShopContext } from '../context/ShopContext'
 
 const Navbar = () => {
